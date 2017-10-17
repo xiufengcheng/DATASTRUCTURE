a9f4cbb01aa52878368f83954390b024d51bbb59
程秀峰 <32472889@qq.com>
2017-10-17 17:28:56 +0800

    改过

--- BIGHOMEWORKS/1/wn_homework/4.cpp
+++ BIGHOMEWORKS/1/wn_homework/4.cpp
@@ -1,52 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <iomanip>
-#include <iostream>
-using namespace std;
-typedef int ElemType;
+//已知Q是一个（不带头结点的）非空队列，S是一个（不带头结点的）空栈
+//编写算法实现：将队列Q中的所有元素逆置。要求：调用头文件操作函数实现。
+//答：元素依次出队列，依次入栈，逆序出栈，依次人队列
+
 #include "SqQueue.h"
 #include "SqStack.h"
 
-void Inverse(SqQueue &Q){
-    SqStack mystack;
-    InitStack_Sq(mystack);
+void Inverse(SqQueue &Q, SqQueue &S){
     while(!QueueEmpty_Sq(Q)){
         ElemType e;
         DeQueue_Sq(Q,e);
-        Push_Sq(mystack,e);
+        Push_Sq(S,e);
     }
-    while(!StackEmpty_Sq(mystack)){
+    while(!StackEmpty_Sq(S)){
         ElemType e;
         Pop_Sq(mystack,e);
         EnQueue_Sq(Q,e);
     }
-}
-
-
-int main()
-{
-    SqQueue myqueue;
-    InitQueue_Sq(myqueue);
-    ElemType e[10]={1,2,3,4,5,6,7};
-    int i=0;
-    while(i<7){
-        EnQueue_Sq(myqueue,e[i++]);
-    }
-    while(!QueueEmpty_Sq(myqueue)){
-        ElemType e;
-        DeQueue_Sq(myqueue,e);
-        cout<<setw(4)<<e;
-    }
-    i=0;
-    while(i<7){
-        EnQueue_Sq(myqueue,e[i++]);
-    }
-    Inverse(myqueue);
-    while(!QueueEmpty_Sq(myqueue)){
-        ElemType e;
-        DeQueue_Sq(myqueue,e);
-        cout<<setw(4)<<e;
-    }
-
-    return 0;
 }
